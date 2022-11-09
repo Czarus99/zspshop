36f063ba51415c7f0b59d05d8f791c8bf35e4863
czarus99 <cezarprokoks@gmail.com>
2022-11-09 12:45:01 +0100

    essa

---
+++ essabaza.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>fajnezakupki.pl</title>
+    </head>
+    <body>
+         <?php
+        $con = new mysqli("localhost", "root", "", "zspshop");
+    </body>
+</html>
