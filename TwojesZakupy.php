0898e863354895517c5593b5fc88cdff7768e2c9
czarus99 <cezarprokoks@gmail.com>
2022-11-10 14:22:38 +0100

    essowka

---
+++ TwojesZakupy.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>fajnezakupki.pl</title>
+        <link rel="stylesheet" href="essabaza.css">
+    </head>
+    <body>
+        <?php
+        $con = new mysqli("localhost", "root", "", "zspshop");
+        ?>
+        <div class="miner">
+            <a href="essabaza.php"><img src="Noeloelo.png" style="width:100px;"></a>
+        </div>
--- essabaza2.php
+++ essabaza2.php
@@ -8,14 +8,17 @@
         <?php
         $con = new mysqli("localhost", "root", "", "zspshop");
         ?>
+        <div class="miner">
+            <a href="essabaza.php"><img src="Noeloelo.png" style="width:100px;"></a>
+        </div>
             <table class="center">
                 <tr>
-                    <th>Twoje oferty</th>
-                    <th>Twoje zakupy</th>
-                    <th>Dodaj oferte</th>
-                    <th>Opinie</th>
-                    <th>Świeże wiadomości</th>
-                    <th>Ale mi sie nie chce</th>
+                    <th><a href="TwojeOferty.php">Twoje oferty</a></th>
+                    <th><a href="TwojesZakupy.php">Twoje zakupy</a></th>
+                    <th><a href="DodajOferte.php">Dodaj oferte</a></th>
+                    <th><a href="Lolessa.php">Opinie</a></th>
+                    <th><a href="Lolessa.php">Świeże Wiadomości</a></th>
+                    <th><a href="Lolessa.php">Ale mi sie nie chce</a></th>
                 </tr>
             </table>
         
